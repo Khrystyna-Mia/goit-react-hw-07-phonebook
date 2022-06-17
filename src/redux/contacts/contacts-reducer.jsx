c4b347de2389d768563f20bc374fa9bcb5dfd5f0
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-06-17 23:23:13 +0300

    refactoring

--- src/redux/contacts/contacts-reducer.jsx
+++ src/redux/contacts/contacts-reducer.jsx
@@ -1,25 +1,18 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { combineReducers } from 'redux';
-import { addContact, deleteContact, filterContact } from './contacts-actions';
+// import { combineReducers } from 'redux';
+import { filterContact } from './contacts-actions';
 
-const initialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const items = createReducer([], {
+//   [addContact]: (state, { payload }) => [...state, payload],
+//   [deleteContact]: (state, { payload }) =>
+//     state.filter(({ id }) => id !== payload),
+// });
 
-const items = createReducer(initialState, {
-  [addContact]: (state, { payload }) => [...state, payload],
-  [deleteContact]: (state, { payload }) =>
-    state.filter(({ id }) => id !== payload),
-});
-
-const filter = createReducer('', {
+export const filter = createReducer('', {
   [filterContact]: (_, { payload }) => payload,
 });
 
-export default combineReducers({
-  items,
-  filter,
-});
+// export default combineReducers({
+//   items,
+//   filter,
+// });
