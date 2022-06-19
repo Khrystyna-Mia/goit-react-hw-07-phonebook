088536a5cfcd329fc049ea76848f89c98a4138d7
Khrystyna Hrekul <88985351+Khrystyna-Mia@users.noreply.github.com>
2022-06-19 18:55:09 +0300

    add RTK Query

--- src/services/contactsApi.jsx
+++ src/services/contactsApi.jsx
@@ -5,12 +5,37 @@ export const contactsApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://62ac950e402135c7acb4cacf.mockapi.io/',
   }),
+
   tagTypes: ['Contacts'],
   endpoints: builder => ({
-    getContactsApi: builder.query({
-      query: () => `contacts`,
+    fetchContacts: builder.query({
+      query: () => `/contacts`,
+      providesTags: ['Contacts'],
+    }),
+
+    addContact: builder.mutation({
+      query: newContact => ({
+        url: '/contacts',
+        method: 'POST',
+        body: newContact,
+      }),
+
+      invalidatesTags: ['Contacts'],
+    }),
+
+    deleteContact: builder.mutation({
+      query: contactId => ({
+        url: `/contacts/${contactId}`,
+        method: 'DELETE',
+      }),
+
+      invalidatesTags: ['Contacts'],
     }),
   }),
 });
 
-export const { useGetContactsApiQuery } = contactsApi;
+export const {
+  useFetchContactsQuery,
+  useAddContactMutation,
+  useDeleteContactMutation,
+} = contactsApi;
